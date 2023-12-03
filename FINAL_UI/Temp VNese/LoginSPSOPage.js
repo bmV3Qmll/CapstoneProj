5b135df7ba1c9c6de646f9710a586c78bf041b2c
Namvt2003 <113272680+Namvt2003@users.noreply.github.com>
2023-12-03 05:28:31 -0800

    Update LoginSPSOPage.js

--- FINAL_UI/Temp VNese/LoginSPSOPage.js
+++ FINAL_UI/Temp VNese/LoginSPSOPage.js
@@ -1,45 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var groupContainer = document.getElementById("groupContainer");
-    var frameContainer = document.getElementById("frameContainer");
-  
-    if (groupContainer) {
-      groupContainer.addEventListener("click", function () {
+  var groupContainer = document.getElementById("groupContainer");
+  var frameContainer = document.getElementById("frameContainer");
+
+  if (groupContainer) {
+    groupContainer.addEventListener("click", function () {
+     
+      authenticateUser();
+    });
+  }
+
+  if (frameContainer) {
+    frameContainer.addEventListener("click", function () {
        
-        authenticateUser();
-      });
+      authenticateUser();
+    });
+  }
+
+  function authenticateUser() {
+    var usernameInput = document.getElementById("usernameInput");
+    var passwordInput = document.getElementById("passwordInput");
+
+    // Replace these values with your true username and password
+    var trueUsername = "admin";
+    var truePassword = "admin123";
+
+    var enteredUsername = usernameInput.value;
+    var enteredPassword = passwordInput.value;
+
+    if (enteredUsername === trueUsername && enteredPassword === truePassword) {
+       
+      
+      window.location.href = "./SPSOMain.html";
+  } 
+  else if(enteredUsername == '' && enteredPassword === '') {
+    alert("Xin hãy nhập tên đăng nhập và mật khẩu")
+      return;
+  } 
+    else {
+       
+      alert("Xác minh không thành công. Vui lòng nhập lại tên đăng nhập hoặc mật khẩu.");
+      return;
     }
-  
-    if (frameContainer) {
-      frameContainer.addEventListener("click", function () {
-         
-        authenticateUser();
-      });
-    }
-  
-    function authenticateUser() {
-      var usernameInput = document.getElementById("usernameInput");
-      var passwordInput = document.getElementById("passwordInput");
-  
-      // Replace these values with your true username and password
-      var trueUsername = "admin";
-      var truePassword = "admin123";
-  
-      var enteredUsername = usernameInput.value;
-      var enteredPassword = passwordInput.value;
-  
-      if (enteredUsername === trueUsername && enteredPassword === truePassword) {
-        // Authentication successful, redirect to the main page
-        
-        window.location.href = "./SPSOMain.html";
-    } 
-    else if(enteredUsername == '' && enteredPassword === '') {
-        return;
-    } 
-      else {
-        // Authentication failed, you can handle this accordingly (show an error message, for example)
-        alert("Xác minh không thành công. Vui lòng nhập lại tên đăng nhập hoặc mật khẩu.");
-        return;
-      }
-    }
-  });
-  
+  }
+
+  var usernameInput = document.getElementById("usernameInput");
+var passwordInput = document.getElementById("passwordInput");
+
+if (usernameInput) {
+  usernameInput.addEventListener("keydown", handleEnter);
+}
+
+if (passwordInput) {
+  passwordInput.addEventListener("keydown", handleEnter);
+}
+
+function handleEnter(e) {
+  if (e.keyCode == 13) {
+    authenticateUser();
+  }
+}
+});
+ 
+
+
