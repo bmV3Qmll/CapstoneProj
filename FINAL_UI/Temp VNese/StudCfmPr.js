88afedf7745f026cd9068c0327b48d3b73ff273e
illubaby <aironb177@gmail.com>
2023-12-04 21:02:38 +0700

    final

--- FINAL_UI/Temp VNese/StudCfmPr.js
+++ FINAL_UI/Temp VNese/StudCfmPr.js
@@ -1,16 +1,23 @@
 document.addEventListener("DOMContentLoaded",function(){
-    var Addbutton = document.getElementById("Addbutton");
+    var Addbutton = document.getElementById("Addbutton1");
+    var Addbutton2 = document.getElementById("Addbutton2");
     if (Addbutton){
         Addbutton.addEventListener("click",function(){
             alert("Đã chọn mặc định");
         });
     }
-});
-document.addEventListener("DOMContentLoaded",function(){
-    var Addbutton = document.getElementById("Addbutton");
-    if (Addbutton){
-        Addbutton.addEventListener("click",function(){
-            alert("Đã chọn mặc định");
+    if (Addbutton2){
+        Addbutton2.addEventListener("click",function(){
+            alert("In thành công !");
         });
     }
+    
 });
+// document.addEventListener("upload-files-item",function(){
+//     var Addbutton2 = document.getElementById("Addbutton2");
+//     if (Addbutton2){
+//         Addbutton2.addEventListener("click",function(){
+//             alert("In thành công !");
+//         });
+//     }
+// });
